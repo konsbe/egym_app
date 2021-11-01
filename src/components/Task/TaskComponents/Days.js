cce488238b873f81df329db8a8d79113c737f9b3
konsbe <kons.be@outlook.com>
2021-11-02 01:46:47 +0200

    fix some bugs

--- src/components/Task/TaskComponents/Days.js
+++ src/components/Task/TaskComponents/Days.js
@@ -8,9 +8,10 @@ import { CSSTransition } from "react-transition-group";
 
 import { useSelector } from "react-redux";
 
+import { checkUserIsAdmin } from "../../../Utils";
+
 const mapState = ({ user }) => ({
   currentUser: user.currentUser,
-  users: user.currentUser,
 });
 
 const Days = ({ week, onDelete }) => {
@@ -59,54 +60,93 @@ const Days = ({ week, onDelete }) => {
     console.log(weekProgram);
   };
 
-  return (
-    <div className="containerone">
-      <h2 className="weekHeader" onClick={() => setShowWeek(!showWeek)}>
-        {week.text}
-      </h2>
-      <CSSTransition
-        // in={true}
-        nodeRef={nodeRef}
-        in={showWeek}
-        appear={true}
-        timeout={500}
-        classNames="transition"
-        unmountOnExit
-        // unmountOnEnter
-      >
-        <div ref={nodeRef}>
-          <div className="containerone">
-            {currentUser.userRoles[1] && (
-              <div>
-                <AddDay onAdd={addDay} />
-                <button
-                  className="weekDelete btn-block"
-                  onClick={() => onDelete(week.id)}
-                >
-                  Delete
-                </button>
-              </div>
-            )}
-          </div>
-          {days.map((day) => (
-            <Day
-              key={day.id}
-              day={day}
-              onDelete={deleteDay}
-              onClick={handleonClick}
-              func={pull_data}
-            />
-          ))}
-          <button onClick={handleonClick} className="btnAdd btn-block">
-            Add
-          </button>
-          <button onClick={handleAddClick} className="btnAddWeek btn-block">
-            Add
-          </button>
-        </div>
-      </CSSTransition>
-    </div>
-  );
-};
+  const isAdmin = checkUserIsAdmin(currentUser);
 
+  if (isAdmin) {
+    return (
+      <div className="containerone">
+        <h2 className="weekHeader" onClick={() => setShowWeek(!showWeek)}>
+          {week.text}
+        </h2>
+        <CSSTransition
+          // in={true}
+          nodeRef={nodeRef}
+          in={showWeek}
+          appear={true}
+          timeout={500}
+          classNames="transition"
+          unmountOnExit
+          // unmountOnEnter
+        >
+          <div ref={nodeRef}>
+            <div className="containerone">
+              {currentUser.userRoles[1] && (
+                <div>
+                  <AddDay onAdd={addDay} />
+                  <button
+                    className="weekDelete btn-block"
+                    onClick={() => onDelete(week.id)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              )}
+            </div>
+            {days.map((day) => (
+              <Day
+                key={day.id}
+                day={day}
+                onDelete={deleteDay}
+                onClick={handleonClick}
+                func={pull_data}
+              />
+            ))}
+            <button onClick={handleonClick} className="btnAdd btn-block">
+              Add
+            </button>
+            <button onClick={handleAddClick} className="btnAddWeek btn-block">
+              Add
+            </button>
+          </div>
+        </CSSTransition>
+      </div>
+    );
+  } else {
+    return (
+      <div className="containerone">
+        <h2 className="weekHeader" onClick={() => setShowWeek(!showWeek)}>
+          {week.text}
+        </h2>
+        <CSSTransition
+          // in={true}
+          nodeRef={nodeRef}
+          in={showWeek}
+          appear={true}
+          timeout={500}
+          classNames="transition"
+          unmountOnExit
+          // unmountOnEnter
+        >
+          <div ref={nodeRef}>
+            {days.map((day) => (
+              <Day
+                key={day.id}
+                day={day}
+                onDelete={deleteDay}
+                onClick={handleonClick}
+                func={pull_data}
+              />
+            ))}
+            <button onClick={handleonClick} className="btnAdd btn-block">
+              Add
+            </button>
+            <button onClick={handleAddClick} className="btnAddWeek btn-block">
+              Add
+            </button>
+          </div>
+        </CSSTransition>
+      </div>
+    );
+  }
+};
 export default Days;
--- src/components/Task/TaskComponents/Weeks.js
+++ src/components/Task/TaskComponents/Weeks.js
@@ -8,15 +8,15 @@ import AddWeek from "../AddWeek";
 
 
 import { useSelector } from "react-redux";
+import { checkUserIsAdmin } from "../../../Utils";
 
 const mapState = ({ user }) => ({
   currentUser: user.currentUser,
-  users: user.currentUser,
 });
 
 const Weeks = ({ onDelete }) => {
   const { currentUser } = useSelector(mapState);
-  //   const nodeRef = useRef(null);
+
   //   const [showWeek, setShowWeek] = useState(false);
   const [weeks, setWeeks] = useState([
     {
@@ -38,30 +38,48 @@ const Weeks = ({ onDelete }) => {
     setWeeks(weeks.filter((week) => week.id !== id));
   };
 
-  return (
-    <>
-      {currentUser.userRoles[1] && (
+  const isAdmin = checkUserIsAdmin(currentUser); //   const nodeRef = useRef(null);
+  if (isAdmin) {
+    return (
+      <div>
         <div className="containerWeek">
           <AddWeek onAdd={addWeek} onDelete={deleteWeek} />
         </div>
-      )}
 
-      <div
-        className="weeksContainer"
-        //   onClick={() => setShowWeek(!showWeek)}
-      >
-        {weeks.map((week) => (
-          <Days
-            onDelete={onDelete}
-            key={week.id}
-            week={week}
-            onDelete={deleteWeek}
-          />
-        ))}
+        <div
+          className="weeksContainer"
+          //   onClick={() => setShowWeek(!showWeek)}
+        >
+          {weeks.map((week) => (
+            <Days
+              onDelete={onDelete}
+              key={week.id}
+              week={week}
+              onDelete={deleteWeek}
+            />
+          ))}
+        </div>
       </div>
-      {/* </div> */}
-    </>
-  );
+    );
+  } else {
+    return (
+      <div>
+        <div
+          className="weeksContainer"
+          //   onClick={() => setShowWeek(!showWeek)}
+        >
+          {weeks.map((week) => (
+            <Days
+              onDelete={onDelete}
+              key={week.id}
+              week={week}
+              onDelete={deleteWeek}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Weeks;
