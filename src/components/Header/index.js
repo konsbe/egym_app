d6f5c53fdf96e7049ea07ccd306797b4cad9757c
konsbe <kons.be@outlook.com>
2021-11-06 16:32:48 +0200

    phone almost done ??

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,6 +10,10 @@ import "./styles.css";
 import { Link, useHistory } from "react-router-dom";
 import { Navbar, Nav, NavDropdown, Button } from "react-bootstrap";
 
+import { SideBarDataLogIn, SideBarDataLogOut } from "./SideBarData";
+
+import { FaBars } from "@react-icons/all-files/fa/FaBars";
+import { AiOutlineClose } from "@react-icons/all-files/ai/AiOutlineClose";
 import snatch from "./../../assets/snatch.jpg";
 
 const mapState = ({ user }) => ({
@@ -21,6 +25,8 @@ const Header = (props) => {
   const dispatch = useDispatch();
   const history = useHistory();
   const [scrolled, setScrolled] = useState(false);
+  const [sidebar, setSidebar] = useState(false);
+  const showSideBar = () => setSidebar(!sidebar);
   // const userID = useAuth();
   const { currentUser } = useSelector(mapState);
   const { users } = useSelector(mapState);
@@ -56,129 +62,176 @@ const Header = (props) => {
   // }, []);
 
   return (
-    <nav sticky="top" onScroll={handleScroll}>
-      <Navbar
-        collapseOnSelect
-        expand="lg"
-        // bg="dark"
-        variant="dark"
-        fixed="top"
-        sticky="top"
-        // onScroll={handleScroll}
-        // className="navigation-bar"
-        className={x.join(" ")}
-      >
-        <Navbar.Brand
-          style={{ marginLeft: 10 }}
-          className="navbarBrand"
-          href="/"
+    <div>
+      <nav sticky="top" onScroll={handleScroll} className="computerBar">
+        <Navbar
+          collapseOnSelect
+          expand="lg"
+          // bg="dark"
+          variant="dark"
+          fixed="top"
+          sticky="top"
+          // onScroll={handleScroll}
+          // className="navigation-bar"
+          className={x.join(" ")}
         >
-          <div className="wrap">
-            <img className="brand-image" src={snatch} alt="Brand Logo" />
-          </div>
-        </Navbar.Brand>
-        <Navbar.Toggle
-          className="burger"
-          aria-controls="responsive-navbar-nav"
-        />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto pc_display">
-            <Nav.Link as={Link} to="/" className="navmenu">
-              <span className={y.join(" ")}>HOME</span>
-            </Nav.Link>
-            <Nav.Link as={Link} to="/courses" className="navmenu">
-              <span className={y.join(" ")}>COURSES</span>
-            </Nav.Link>
-            <Nav.Link as={Link} to="/test" className="navmenu">
-              <span className={y.join(" ")}>TEST</span>
-            </Nav.Link>
-            <NavDropdown
-              className="navmenu"
-              title="TEST"
-              id="collasible-nav-dropdown"
-            >
-              <NavDropdown.Item className="submenu">Action</NavDropdown.Item>
-              <NavDropdown.Item className="submenu">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item className="submenu">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item className="submenu">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            {!currentUser && (
-              <ul>
-                <li>
-                  <Nav.Link as={Link} to="/profile" className="navmenu">
-                    <Button
-                      className="btnlink"
-                      variant="outline-success"
-                      id="btnNavbar"
+          <Navbar.Brand
+            style={{ marginLeft: 10 }}
+            className="navbarBrand"
+            href="/"
+          >
+            <div className="wrap">
+              <img className="brand-image" src={snatch} alt="Brand Logo" />
+            </div>
+          </Navbar.Brand>
+          <Navbar.Toggle
+            className="burger"
+            aria-controls="responsive-navbar-nav"
+          />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto pc_display">
+              <Nav.Link as={Link} to="/" className="navmenu">
+                <span className={y.join(" ")}>HOME</span>
+              </Nav.Link>
+              <Nav.Link as={Link} to="/courses" className="navmenu">
+                <span className={y.join(" ")}>COURSES</span>
+              </Nav.Link>
+              <Nav.Link as={Link} to="/test" className="navmenu">
+                <span className={y.join(" ")}>TEST</span>
+              </Nav.Link>
+              <NavDropdown
+                className="navmenu"
+                title="TEST"
+                id="collasible-nav-dropdown"
+              >
+                <NavDropdown.Item className="submenu">Action</NavDropdown.Item>
+                <NavDropdown.Item className="submenu">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item className="submenu">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item className="submenu">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              {!currentUser && (
+                <ul>
+                  <li>
+                    <Nav.Link as={Link} to="/profile" className="navmenu">
+                      <Button
+                        className="btnlink"
+                        variant="outline-success"
+                        id="btnNavbar"
+                      >
+                        My Profile
+                      </Button>
+                    </Nav.Link>
+                  </li>
+                  <li>
+                    <Nav.Link as={Link} to="/login" className="navmenu">
+                      <Button
+                        className="btnlink"
+                        variant="outline-success"
+                        id="btnNavbar"
+                      >
+                        LOG IN / SIGN UP
+                      </Button>
+                    </Nav.Link>
+                  </li>
+                </ul>
+              )}
+              {currentUser && (
+                <ul>
+                  <li>
+                    <Nav.Link
+                      as={Link}
+                      to={`/user/${users.id}`}
+                      className="navmenu"
                     >
-                      My Profile
-                    </Button>
-                  </Nav.Link>
-                </li>
-                <li>
-                  <Nav.Link as={Link} to="/login" className="navmenu">
-                    <Button
-                      className="btnlink"
-                      variant="outline-success"
-                      id="btnNavbar"
-                    >
-                      LOG IN / SIGN UP
-                    </Button>
-                  </Nav.Link>
-                </li>
-              </ul>
-            )}
-            {currentUser && (
-              <ul>
-                <li>
-                  <Nav.Link
-                    as={Link}
-                    to={`/user/${users.id}`}
-                    className="navmenu"
-                  >
-                    <Button
-                      className="btnlink"
-                      variant="outline-success"
-                      id="btnNavbar"
-                    >
-                      My Profile
-                    </Button>
-                  </Nav.Link>
-                </li>
-                <li>
-                  <Nav.Link className="navmenu">
-                    <Button
-                      className="btnlink"
-                      variant="outline-success"
-                      id="btnNavbar"
-                    >
-                      <span onClick={() => signOut()}>LOGOUT</span>
-                    </Button>
-                  </Nav.Link>
-                </li>
-              </ul>
-            )}
+                      <Button
+                        className="btnlink"
+                        variant="outline-success"
+                        id="btnNavbar"
+                      >
+                        My Profile
+                      </Button>
+                    </Nav.Link>
+                  </li>
+                  <li>
+                    <Nav.Link className="navmenu">
+                      <Button
+                        className="btnlink"
+                        variant="outline-success"
+                        id="btnNavbar"
+                      >
+                        <span onClick={() => signOut()}>LOGOUT</span>
+                      </Button>
+                    </Nav.Link>
+                  </li>
+                </ul>
+              )}
 
-            <Nav.Link as={Link} to="/" className="navmenu nav_burger">
-              HOME
-            </Nav.Link>
-            <Nav.Link as={Link} to="/courses" className="navmenu nav_burger">
-              COURSES
-            </Nav.Link>
-            <Nav.Link as={Link} to="/test" className="navmenu nav_burger">
-              TEST
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </nav>
+              <Nav.Link as={Link} to="/" className="navmenu nav_burger">
+                HOME
+              </Nav.Link>
+              <Nav.Link as={Link} to="/courses" className="navmenu nav_burger">
+                COURSES
+              </Nav.Link>
+              <Nav.Link as={Link} to="/test" className="navmenu nav_burger">
+                TEST
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </nav>
+      <div className="phoneBar">
+        <div className={x.join(" ")} sticky="top" onScroll={handleScroll}>
+          <Link to="#" className="menu-bars">
+            <FaBars onClick={showSideBar} />
+          </Link>
+        </div>
+        {sidebar && (
+          <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+            <ul className="nav-menu-items" onClick={showSideBar}>
+              <li className="navbar-toggle">
+                <div onClick={showSideBar} className="menu-bars">
+                  <AiOutlineClose />
+                </div>
+              </li>
+              {SideBarDataLogOut.map((item, index) => {
+                {
+                  if (index === 3) {
+                    return (
+                      <li key={index} className={item.cname}>
+                        <Link to={"/user/${users.id}"}>
+                          <span>{item.title}</span>
+                          <span className="icon">{item.icon}</span>
+                          {/* {item.icon} */}
+                        </Link>
+                      </li>
+                    );
+                  } else {
+                    return (
+                      <li key={index} className={item.cname}>
+                        <Link to={item.path}>
+                          <span>{item.title}</span>
+                          <span className="icon">{item.icon}</span>
+                          {/* {item.icon} */}
+                        </Link>
+                      </li>
+                    );
+                  }
+                }
+              })}
+            </ul>
+          </nav>
+        )}
+      </div>
+    </div>
   );
 };
 
--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./../components/Header";
+import Navbar from "./../components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from "../components/Footer";
 import "./styles.css";
