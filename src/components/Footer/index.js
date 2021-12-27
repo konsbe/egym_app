c3b5c049aba44b202c22ecc1528afde90bb7fa68
konsbe <kons.be@outlook.com>
2021-12-27 17:12:09 +0200

    fix the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./styles.css";
+import { Link, useHistory } from "react-router-dom";
 
 const Footer = (props) => {
+  const history = useHistory();
   return (
     <footer className="footer" sticky="bottom">
-      <div>© Copyrights to Us</div>
+      <div>
+        <p>© Copyrights to Berdelis Konstantinos</p>
+
+        <p>© email: berdelis.konstantinos@gmail.com</p>
+        <p>
+          © App-Specs:{" "}
+          <Link className="loginlink" to="/app-specs">
+            {" "}
+            app-specs{" "}
+          </Link>
+        </p>
+      </div>
     </footer>
   );
 };
